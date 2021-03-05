fa5f12717130aa50bc0138363b02566b7a2aba45
Personal Mac <personalmac@Personals-MacBook-Pro.local>
2021-03-04 19:18:23 -0500

    Deploy to Heroku

--- app/ml.py
+++ app/ml.py
@@ -2,7 +2,7 @@
 
 import logging
 import random
-
+import numpy as np 
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
@@ -14,43 +14,42 @@ router = APIRouter()
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    zipcode: int = Field(..., example=99205)
+    family_members: int = Field(..., example= 4)
+    income: int = Field(..., example= 4000)
 
-    def to_df(self):
-        """Convert pydantic object to pandas dataframe with 1 row."""
-        return pd.DataFrame([dict(self)])
+    #def to_df(self):
+       # """Convert pydantic object to pandas dataframe with 1 row."""
+        #return pd.DataFrame([dict(self)])
 
-    @validator('x1')
-    def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
-        return value
-
+    #@validator('x1')
+    #def x1_must_be_positive(cls, value):
+        #"""Validate that x1 is a positive number."""
+        #assert value > 0, f'x1 == {value}, must be > 0'
+        #return value
 
 @router.post('/predict')
-async def predict(item: Item):
-    """
-    Make random baseline predictions for classification problem 🔮
+#async def predict(item: Item):
+async def determine_eligibility(item: Item):
 
-    ### Request Body
-    - `x1`: positive float
-    - `x2`: integer
-    - `x3`: string
+    zips = pd.read_csv('app/spokane_zipcodes.csv')
 
-    ### Response
-    - `prediction`: boolean, at random
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
+    user = zips[zips['zipcode'] == item.zipcode]
+    user = user[user['family_members'] == item.family_members]
+    user_income = item.income*12
+    comp_income = user['annual_income'].astype(np.int32)
+        
+    if (user_income <= user['annual_income']).all():
+        results =  'You Qualify!'
+    else:
+        results = 'Application Pending - income exceeds limit'
 
-    Replace the placeholder docstring and fake predictions with your own model.
-    """
-    X_new = item.to_df()
-    log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
+    #X_new = item.to(_df()
+    #log.info(X_new)
+    #y_pred = random.choice([True, False])
+    #y_pred_proba = random.random() / 2 + 0.5
     return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
+        #'prediction': y_pred,
+        #'probability': y_pred_proba
+        'eligibility': results
     }
